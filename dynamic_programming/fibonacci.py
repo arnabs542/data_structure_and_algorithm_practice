8734111a7b197d17bab858492d630a6990970d85
Michael Mroczka <michael.mroczka@gmail.com>
2018-08-30 11:29:51 -0400

    Add top down DP fibonacci solution

--- dynamic_programming/fibonacci.py
+++ dynamic_programming/fibonacci.py
@@ -12,19 +12,36 @@ def nth_fibonacci(n):
     else:
         return nth_fibonacci(n-1) + nth_fibonacci(n-2)
 
+def nth_fibonacci_DP_top_down(n, memo=None):
+    if n < 1:
+        raise AssertionError
 
+    if memo == None:
+        memo = dict()
+
+    if n in [1, 2]:
+        return 1
+    else:
+        if n in memo:
+            return memo[n]
+        key = nth_fibonacci_DP_top_down(n-1, memo) + nth_fibonacci_DP_top_down(n-2, memo)
+        memo[n] = key
+        return key
 
 class FibonacciTest(unittest.TestCase):
     def test_one(self):
         self.assertEqual(nth_fibonacci(1), 1)
+        self.assertEqual(nth_fibonacci_DP_top_down(1), 1)
         print("Passed ")
 
     def test_two(self):
         self.assertEqual(nth_fibonacci(2), 1)
+        self.assertEqual(nth_fibonacci_DP_top_down(2), 1)
         print("Passed")
 
     def test_ten(self):
-        self.assertEqual(nth_fibonacci(1), 1)
+        self.assertEqual(nth_fibonacci(10), 55)
+        self.assertEqual(nth_fibonacci_DP_top_down(10), 55)
         print("Passed")
 
 test_nth_fibonacci = FibonacciTest()
