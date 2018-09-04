fcd10db34f12bac4513084918ef2e43482ca732a
Michael Mroczka <michael.mroczka@gmail.com>
2018-09-04 11:00:32 -0400

    Add palindromic sub-string length DP problem

---
+++ dynamic_programming/palindromic_sub_string_length.py
@@ -0,0 +1,16 @@
+# Given a string, find the length of the maximum length palindromic sub string .
+# Suppose 'l' is the length of maximum length palindromic sub string, check whether 'l' is prime number or not,
+# if it is prime ,then print 'PRIME' else 'NOT PRIME'.
+#
+# SAMPLE INPUT
+# 3
+# aabaaz
+# aavsba
+# ghsdffssff
+#
+# SAMPLE OUTPUT
+# PRIME
+# PRIME
+# NOT PRIME
+
+
