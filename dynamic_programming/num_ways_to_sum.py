57b0c6901937d5300949260c66339ae5d0c3e6fc
Michael Mroczka <michaelmroczka@gmail.com>
2018-09-06 22:36:38 -0400

    Solved problem brute force recursively then top down DP style :)

--- dynamic_programming/num_ways_to_sum.py
+++ dynamic_programming/num_ways_to_sum.py
@@ -1,15 +1,47 @@
-
+from datetime import datetime, timedelta
+import unittest
 
 def num_ways_to_sum(n, numbers):
-    ways = 0
-    ways += num_ways_to_sum_rec(n, numbers)
-    print(ways)
-
-def num_ways_to_sum_rec(n, numbers):
+    totals = 0
     for num in numbers:
         if n - num == 0:
-            return 1
+            totals += 1
         elif n - num > 0:
-            return num_ways_to_sum_rec(n-num, numbers) + num_ways_to_sum_rec(n-num, numbers)
+            totals += num_ways_to_sum(n-num, numbers)
+    return totals
 
-num_ways_to_sum(5, [1,3,4])
+def num_ways_to_sum_DP_top_down(n, numbers, memo=None):
+    totals = 0
+
+    if memo == None:
+        memo = dict()
+
+    if n in memo:
+        return memo[n]
+
+    for num in numbers:
+        if n - num == 0:
+            totals += 1
+        elif n - num > 0:
+            totals += num_ways_to_sum_DP_top_down(n-num, numbers, memo)
+    memo[n] = totals
+    return totals
+
+class SumTest(unittest.TestCase):
+    def test_example_one(self):
+        self.assertEqual(num_ways_to_sum(5,[1,3,4]), 6)
+        self.assertEqual(num_ways_to_sum_DP_top_down(5,[1,3,4]), 6)
+        print("Passed")
+
+    def test_sum_times(self):
+        start_time = datetime.now()
+        num_ways_to_sum(35,[1,3,4])
+        print("Brute force number of ways to sum = " + str(datetime.now() - start_time))
+
+        start_time = datetime.now()
+        num_ways_to_sum_DP_top_down(35,[1,3,4])
+        print("Brute force number of ways to sum w/DP top down = " + str(datetime.now() - start_time))
+
+test_sums = SumTest()
+test_sums.test_example_one()
+test_sums.test_sum_times()
