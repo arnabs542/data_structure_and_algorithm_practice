b08c99402858e5904c7e511270d872ba777a1119
Michael Mroczka <michael.mroczka@gmail.com>
2018-09-12 10:46:50 -0400

    Add learning longest common subsequence algorithm & question setup

---
+++ dynamic_programming/longest_common_subsequence.py
@@ -0,0 +1,9 @@
+# Given two strings find the longest common subsequence between the two.
+# A subsequence does not have to be consecutive.
+# Example longest_common_subsequence("abcdaf", "acbcf") would return 4
+# The subsequence would be "abcf" common among both input strings.
+
+def longest_common_subsequence(string_one, string_two):
+    pass
+
+
