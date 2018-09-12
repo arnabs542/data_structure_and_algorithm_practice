50db74aca25ef880b4d2386a8ecdf3b25d6476c9
Michael Mroczka <michael.mroczka@gmail.com>
2018-09-12 11:47:39 -0400

    Derived longest common subsequence algorithm - not pretty

--- dynamic_programming/longest_common_subsequence.py
+++ dynamic_programming/longest_common_subsequence.py
@@ -4,6 +4,31 @@
 # The subsequence would be "abcf" common among both input strings.
 
 def longest_common_subsequence(string_one, string_two):
-    pass
+    # build an empty 2D array of size [string_one] * [string_two]
+    two_d_array = [[0] * len(string_two) for i in range(len(string_one))]
 
+    longest = 0
+    for row in range(len(string_one)):
+        for col in range(len(string_two)):
+            if string_one[row] == string_two[col]:
+                current_longest = 0
+                if row-1 >= 0 and col-1 >= 0:
+                    current_longest = two_d_array[row-1][col-1]
+                two_d_array[row][col] = current_longest + 1
+                if current_longest + 1 > longest:
+                    longest = current_longest + 1
+            else:
+                try:
+                    cur_row_max = two_d_array[row-1][col]
+                except IndexError:
+                    cur_row_max = 0
+                try:
+                    cur_col_max = two_d_array[row][col-1]
+                except IndexError:
+                    cur_col_max = 0
+                two_d_array[row][col] = max(cur_row_max, cur_col_max)
+    print(longest)
+
+longest_common_subsequence("abcdaf", "acbcf")
+longest_common_subsequence("abcdefgizzzzaj", "abcdefghiabcdefgh")
 
