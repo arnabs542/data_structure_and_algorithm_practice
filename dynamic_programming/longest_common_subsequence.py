2ff967a370e64f417337e8e5d49c76070a77966d
Michael Mroczka <michael.mroczka@gmail.com>
2018-09-14 10:33:07 -0400

    Add tests to LCS and timer

--- dynamic_programming/longest_common_subsequence.py
+++ dynamic_programming/longest_common_subsequence.py
@@ -1,9 +1,11 @@
+from datetime import datetime, timedelta
+import unittest
 # Given two strings find the longest common subsequence between the two.
 # A subsequence does not have to be consecutive.
 # Example longest_common_subsequence("abcdaf", "acbcf") would return 4
 # The subsequence would be "abcf" common among both input strings.
 
-def longest_common_subsequence(string_one, string_two):
+def longest_common_subsequence_old(string_one, string_two):
     # build an empty 2D array of size [string_one] * [string_two]
     two_d_array = [[0] * len(string_two) for i in range(len(string_one))]
 
@@ -29,7 +31,7 @@ def longest_common_subsequence(string_one, string_two):
                 two_d_array[row][col] = max(cur_row_max, cur_col_max)
     print(longest)
 
-def longest_common_subsequence_2(string_one, string_two):
+def longest_common_subsequence(string_one, string_two):
     # build an empty 2D array of size [string_one] * [string_two]
     two_d_array = [[0] * (len(string_two)+1) for i in range(len(string_one) + 1)]
 
@@ -42,8 +44,31 @@ def longest_common_subsequence_2(string_one, string_two):
                     longest = two_d_array[row][col]
             else:
                 two_d_array[row][col] = max(two_d_array[row-1][col], two_d_array[row][col-1])
-    print(longest)
+    return longest
 
-longest_common_subsequence_2("abcdaf", "acbcf")
-longest_common_subsequence_2("abcdefgizzzzaj", "abcdefghiabcdefgh")
 
+class LCS_Test(unittest.TestCase):
+    def test_one(self):
+        self.assertEqual(longest_common_subsequence("abcdaf", "acbcf"), 4)
+        print("Passed ")
+        self.assertEqual(longest_common_subsequence("abcdefgizzzzaj", "abcdefghiabcdefgh"), 9)
+        print("Passed ")
+        self.assertEqual(longest_common_subsequence("ABCDGH", "AEDFHR"), 3)
+        print("Passed ")
+        self.assertEqual(longest_common_subsequence("AGGTAB", "GXTXAYB"), 4)
+        print("Passed ")
+        self.assertEqual(longest_common_subsequence("acbaed", "abcadf"), 4)
+        print("Passed ")
+        self.assertEqual(longest_common_subsequence("classical", "musical"), 5)
+        print("Passed ")
+        self.assertEqual(longest_common_subsequence("12341", "341213"), 3)
+        print("Passed ")
+
+    def test_timed(self):
+        start_time = datetime.now()
+        longest_common_subsequence("abcdefgizzzzaj", "abcdefghiabcdefgh")
+        print("LCS time = " + str(datetime.now() - start_time))
+
+tests = LCS_Test()
+tests.test_one()
+tests.test_timed()
