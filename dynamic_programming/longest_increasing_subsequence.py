5dfc4ca2bf1a0c765a694792d66739fea9866ecb
Michael Mroczka <michael.mroczka@gmail.com>
2018-09-20 13:13:20 -0400

    Add longest increasing subsequence problem description

---
+++ dynamic_programming/longest_increasing_subsequence.py
@@ -0,0 +1,4 @@
+# find the length of the longest increasing subsequence in a given inpu
+# e.g. given the sequence [3, 4, -1, 0, 6, 2, 3]
+# the length of the longest subsequence is 4 because of [-1 , 0, 2, 3]
+
