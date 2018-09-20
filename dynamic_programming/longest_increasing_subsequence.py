58932858fbe4c70c4918fbdea324e7accceb710e
Michael Mroczka <michael.mroczka@gmail.com>
2018-09-20 13:41:11 -0400

    Tried brute forcing longest increasing subsequence - doesn't work yet

--- dynamic_programming/longest_increasing_subsequence.py
+++ dynamic_programming/longest_increasing_subsequence.py
@@ -2,3 +2,20 @@
 # e.g. given the sequence [3, 4, -1, 0, 6, 2, 3]
 # the length of the longest subsequence is 4 because of [-1 , 0, 2, 3]
 
+def longest_increasing_subsequence_brute_force(a):
+    longest = float('-inf')
+    for i in range(len(a)):
+        count = 1
+        j = i
+        kept = j
+        while j+1 < len(a):
+            if a[kept] < a[j+1]:
+                count = count + 1
+                kept = j+1
+            j = j+1
+        if longest < count:
+            longest = count
+
+    return longest
+
+print(longest_increasing_subsequence_brute_force([3,4,-1,0,6,2,3]))
