990b418a700a6d8aebf2d1d3771d23616e0d5510
Michael Mroczka <michael.mroczka@gmail.com>
2018-09-26 11:31:18 -0400

    Working on recurrance relation for longest increasing subsequence

--- dynamic_programming/longest_increasing_subsequence.py
+++ dynamic_programming/longest_increasing_subsequence.py
@@ -4,18 +4,17 @@
 
 def longest_increasing_subsequence_brute_force(a):
     longest = float('-inf')
-    for i in range(len(a)):
-        count = 1
-        j = i
-        kept = j
-        while j+1 < len(a):
-            if a[kept] < a[j+1]:
-                count = count + 1
-                kept = j+1
-            j = j+1
-        if longest < count:
-            longest = count
 
-    return longest
+    for num in a:
+
+
+def longest_increasing_subsequence_brute_force_rec(list, position):
+    if position > len(list)-1:
+        return 0
+    best = float('-inf')
+    for pos in range(position+1, len(list)-1):
+        if list[position] < list[pos]:
+            return 1 + longest_increasing_subsequence_brute_force_rec(list, position+1)
+    return 0
 
 print(longest_increasing_subsequence_brute_force([3,4,-1,0,6,2,3]))
