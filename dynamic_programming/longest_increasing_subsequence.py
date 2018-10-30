252eb5b6dfb3c697934905464e8f853a25e96c46
Michael Mroczka <michael.mroczka@gmail.com>
2018-10-30 18:43:13 -0400

    Add solution to longest increasing subsequence problem

--- dynamic_programming/longest_increasing_subsequence.py
+++ dynamic_programming/longest_increasing_subsequence.py
@@ -1,20 +1,25 @@
-# find the length of the longest increasing subsequence in a given inpu
+# find the length of the longest increasing subsequence in a given string
 # e.g. given the sequence [3, 4, -1, 0, 6, 2, 3]
 # the length of the longest subsequence is 4 because of [-1 , 0, 2, 3]
 
-def longest_increasing_subsequence_brute_force(a):
-    longest = float('-inf')
+def longest_increasing_subsequence(arr):
+    current_longest = [1]*len(arr)
 
-    for num in a:
+    i = 1
+    while i < len(arr):
+        j=0
+        while j < i:
+            if arr[j] < arr[i]:
+                current_longest[i] = max(current_longest[i], current_longest[j]+1)
+            j+=1
+        i+=1
+
+    longest = 0
+
+    for length in current_longest:
+        if length > longest:
+            longest = length
+    return longest
 
 
-def longest_increasing_subsequence_brute_force_rec(list, position):
-    if position > len(list)-1:
-        return 0
-    best = float('-inf')
-    for pos in range(position+1, len(list)-1):
-        if list[position] < list[pos]:
-            return 1 + longest_increasing_subsequence_brute_force_rec(list, position+1)
-    return 0
-
-print(longest_increasing_subsequence_brute_force([3,4,-1,0,6,2,3]))
+print(longest_increasing_subsequence([3,4,-1,0,6,2,3]))
