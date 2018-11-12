5d9f44eb7441a26027c654447aab080d62b04f82
Michael Mroczka <michaelmroczka@gmail.com>
2018-11-12 15:00:55 -0500

    Add second attempt at validating a BST

--- trees/is_binary_search_tree.py
+++ trees/is_binary_search_tree.py
@@ -29,16 +29,27 @@ def checkBST_rec(node, parent_max=None, parent_min=None):
         return True
     return False
 
+def is_bst(root, last=float('-inf')):
+    if root != None:
+        if root.left:
+            return is_bst(root.left, last)
+        if last < root.data:
+            last = root.data
+        else:
+            return False
+        if root.right:
+            return is_bst(root.right, last)
+        return True
 
-# tree = BinarySearchTree()
-# t = int(input())
-#
-# arr = list(map(int, input().split()))
-#
-# for i in range(t):
-#     tree.create(arr[i])
-#
-# print(str(checkBST(tree.root)))
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+print(str(is_bst(tree.root)))
 
 # tree = BinarySearchTree()
 # tree.create(8)
@@ -51,16 +62,44 @@ def checkBST_rec(node, parent_max=None, parent_min=None):
 #
 # print(str(checkBST(tree.root)))
 
+
+tree = BinarySearchTree()
+tree.create(1)
+tree.root.right = Node(2)
+tree.root.right.right = Node(4)
+tree.root.right.right.right = Node(3)
+tree.root.right.right.right.right = Node(5)
+tree.root.right.right.right.right.right = Node(6)
+tree.root.right.right.right.right.right.right = Node(7)
+
+
+print(str(is_bst(tree.root)))
+
+
+# tree = BinarySearchTree()
+# tree.create(8)
+# tree.root.left = Node(6)
+# tree.root.left.left = Node(3)
+# tree.root.left.right = Node(7)
+# tree.root.left.left.left = Node(1)
+# tree.root.left.left.right = Node(7)
+# tree.root.left.left.right.left = Node(4)
+
+print(str(is_bst(tree.root)))
+
 tree = BinarySearchTree()
 tree.create(8)
 tree.root.left = Node(6)
 tree.root.left.left = Node(3)
 tree.root.left.right = Node(7)
 tree.root.left.left.left = Node(1)
-tree.root.left.left.right = Node(7)
-tree.root.left.left.right.left = Node(4)
+tree.root.left.left.right = Node(4)
+tree.root.right = Node(27)
+tree.root.right.right = Node(37)
+tree.root.right.right.left = Node(4)
 
-print(str(checkBST(tree.root)))
+print(str(is_bst(tree.root)))
+
 '''
 SAMPLE INPUT
 6
