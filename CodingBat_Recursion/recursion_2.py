564e16fb9eab6f8eface712660c3cea7209aeeee
Michael Mroczka <michaelmroczka@gmail.com>
2018-12-01 11:51:15 -0500

    Add CodingBat recursion 2 file and a couple solutions

---
+++ CodingBat_Recursion/recursion_2.py
@@ -0,0 +1,57 @@
+'''
+
+Given an array of ints, is it possible to choose a group of some of the ints, such that the group sums to the given
+target? This is a classic backtracking recursion problem. Once you understand the recursive backtracking strategy in
+this problem, you can use the same pattern for many problems to search a space of choices. Rather than looking at the
+whole array, our convention is to consider the part of the array starting at index start and continuing to the end of
+the array. The caller can specify the whole array simply by passing start as 0. No loops are needed -- the recursive
+calls progress down the array.
+
+print(groupSum(0, [2, 4, 8], 10) → true
+print(groupSum(0, [2, 4, 8], 14) → true
+print(groupSum(0, [2, 4, 8], 9) → false
+'''
+def groupSum(index, arr, target):
+    if len(arr) ==0:
+        return 0==target
+    if index >= len(arr) or index < 0:
+        return False
+
+    if arr[index]==target:
+        return True
+
+    using_current_index = False
+
+    if arr[index] < target:
+        using_current_index = groupSum(index+1, arr, target-arr[index])
+        if using_current_index:
+            return True
+
+    return groupSum(index+1, arr, target)
+
+# print(groupSum(0, [2, 4, 8], 10))
+# print(groupSum(0, [2, 4, 8], 14))
+# print(groupSum(0, [2, 4, 8], 9))
+# print(groupSum(0, [], 0))
+# print(groupSum(0, [], 20))
+
+'''
+Given an array of ints, is it possible to choose a group of some of the ints, beginning at the start index, such that 
+the group sums to the given target? However, with the additional constraint that all 6's must be chosen. 
+(No loops needed.)
+
+groupSum6(0, [5, 6, 2], 8) → true
+groupSum6(0, [5, 6, 2], 9) → false
+groupSum6(0, [5, 6, 2], 7) → false
+groupSum6(0, [8, 6, 2], 8) → true
+groupSum6(0, [8, 6, 1], 8) → false
+'''
+# def groupSum6(index, arr, target):
+#     if len(arr) == 0:
+#         return 0==target
+#
+#     if index >= len(target) and index < 0:
+#         return False
+#
+#     if arr[index]==target:
+#
