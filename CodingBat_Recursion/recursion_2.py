1a4030df3d34ec55a616d4e8b507d8c2506250a7
Michael Mroczka <michael.mroczka@gmail.com>
2018-12-07 09:42:07 -0500

    Add recursion changes

--- CodingBat_Recursion/recursion_2.py
+++ CodingBat_Recursion/recursion_2.py
@@ -73,4 +73,12 @@ def groupSum6(index, arr, target):
 # print(groupSum6(0, [8, 6, 2], 8))
 # print(groupSum6(0, [8, 6, 1], 8))
 
+'''
+Given an array of ints, is it possible to choose a group of some of the ints, such that the group sums to the given 
+target with this additional constraint: If a value in the array is chosen to be in the group, the value immediately 
+following it in the array must not be chosen. (No loops needed.)
 
+groupNoAdj(0, [2, 5, 10, 4], 12) → true
+groupNoAdj(0, [2, 5, 10, 4], 14) → false
+groupNoAdj(0, [2, 5, 10, 4], 7) → false
+'''
