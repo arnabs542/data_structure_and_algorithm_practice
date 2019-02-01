0b665a2de2b821fdcb7e9ca8fbebfb025a939466
Michael Mroczka <michael.mroczka@gmail.com>
2019-02-01 15:33:31 -0500

    Add edit distance problem

--- dynamic_programming/minimum_edit_distance.py
+++ dynamic_programming/minimum_edit_distance.py
@@ -24,3 +24,47 @@ Edit Distance
 #
 # edit_distance('abcdef', 'azced')
 
+
+def edit_distance_rec(word1, word2, i, j):
+    if i == len(word1):
+        return len(word2)-j
+    if j == len(word2):
+        return len(word1)-i
+
+    if word1[i] == word2[j]:
+        return edit_distance_rec(word1, word2, i+1, j+1)
+
+    return 1+min(edit_distance_rec(word1, word2, i+1, j),
+               edit_distance_rec(word1, word2, i, j+1),
+               edit_distance_rec(word1, word2, i+1, j+1))
+
+def edit_distance(word1, word2):
+    return edit_distance_rec(word1, word2, 0, 0)
+
+# print(edit_distance("ball", "sballz"))
+
+
+# def edit_distance_bottom_up(word1, word2):
+#     dp = [[0]*(len(word2)+1) for i in range(len(word1)+1)]
+#
+#     for i in range(len(word1)+1):
+#         dp[i][0] = i
+#
+#     for j in range(len(word2)+1):
+#         dp[0][j] = j
+#
+#     for i in range(len(word1)-1, -1, -1):
+#         for j in range(len(word2)-1, -1, -1):
+#             if word1[i] == word2[j]:
+#                 dp[i][j] = dp[i+1][j+1]
+#             else:
+#                 dp[i][j] = 1 + min(dp[i+1][j],
+#                                    dp[i+1][j+1],
+#                                    dp[i][j+1])
+#     return dp[0][0]
+#
+# print(edit_distance_bottom_up("ball", "sballz"))
+
+
+'''
+'''
