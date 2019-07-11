4bcbed227d712a2fe792c05da7c06150c9dbf6e9
Michael Mroczka <michael.mroczka@ge.com>
2019-07-11 11:47:54 -0500

    Add search matrix code to graph problem

--- graphs/search_2d_matrix.py
+++ graphs/search_2d_matrix.py
@@ -1,3 +1,4 @@
+from collections import deque
 '''
 Shortest distance between two cells in a matrix or grid
 Given a matrix of N*M order. Find the shortest distance from a source cell to a destination cell, traversing through
@@ -25,3 +26,26 @@ Output :  -1
 
 '''
 
+def is_valid(grid, i, j):
+    if i >= len(grid):
+        return False
+    if j >= len(grid[0]):
+        return False
+    if grid[i][j] == '0':
+        return False
+    return True
+
+def shortest_path_to_dest_cell(grid, source_cell):
+    q = deque()
+    visited = set()
+    distance = dict()
+    q.appendleft(source_cell)
+    directions = [(-1,0), (1,0), (0,1), (0,-1)]
+    while len(q) > 0:
+        row,col = q.pop()
+        grid[row][col] = visited.add((row,col))
+        for d in directions:
+            if is_valid(grid, row+d[0], col+d[1]):
+                q.appendleft((row+d[0],col+d[1]))
+
+    return -1
